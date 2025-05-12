b1c4b5a7d932aaa2e46b390c97b4dc4a8a1dfc02
ethanjiang-1 <146405629+ethanjiang-1@users.noreply.github.com>
2025-05-12 19:29:16 +0000

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ Rails.application.routes.draw do
 
   #------------------------------
 
+  root to: "boards#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
